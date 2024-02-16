e848b8bff30b40664b15205cf6bb44e7f10dbbec
Emir Alper Yildiz <eayildiz@outlook.com>
2024-02-17 02:00:53 +0300

    Getir asamasi eklendi. Ikinci asama gelistirilmeye baslandi.

--- src/islemci.v
+++ src/islemci.v
@@ -14,6 +14,8 @@ module islemci (
     output                      bellek_yaz
 );
 
+anabellek anabellek_(clk, bellek_adres, bellek_oku_veri, bellek_yaz_veri, bellek_yaz);
+
 localparam GETIR        = 2'd0;
 localparam COZYAZMACOKU = 2'd1;
 localparam YURUTGERIYAZ = 2'd2;
@@ -23,9 +25,49 @@ reg [1:0] simdiki_asama_ns;
 reg ilerle_cmb;
 
 reg [`VERI_BIT-1:0] yazmac_obegi [0:`YAZMAC_SAYISI-1];
+reg [`VERI_BIT-1:0] islenecek_buyruk;
 reg [`ADRES_BIT-1:0] ps_r;
+reg [`ADRES_BIT-1:0] ps_ns;
+
+//Mikroislemler yazmaclari
+reg [`VERI_BIT-1:0] kaynak_yazmac_1_veri;
+reg [`VERI_BIT-1:0] kaynak_yazmac_2_veri;
+reg [4:0] sonuc_yazmac;
+reg [2:0] islem_kodu;
 
 always @ * begin
+    case(simdiki_asama_r)
+        
+        GETIR:
+        begin
+            islenecek_buyruk = bellek_oku_veri;  //Islenmesi gereken buyrugun program sayaci bellege gonderilir ve gelenbuyruk bir sonraki aşama için kayıt edilir. 
+            ps_ns = ps_r + 4;   //Getir asamasi istek yapildiktan sonra saatin yukselen kenarinda program sayacini gunceller.
+            simdiki_asama_ns = COZYAZMACOKU;
+        end
+        
+        /*Olası iyilestirmeler
+        * rs1 icin sondan 3. bit 0 veya buyruk 1100111.
+        * 1. bit 1 ise dallanma vardir. 
+        */
+        COZYAZMACOKU:
+        begin
+            case(islenecek_buyruk[6:2])
+                01100:
+                begin
+                    kaynak_yazmac_2_veri = yazmac_obegi[islenecek_buyruk[24:20]];
+                    kaynak_yazmac_1_veri = yazmac_obegi[islenecek_buyruk[19:15]];
+
+                    sonuc_yazmac = islenecek_buyruk[11:7];
+                    
+                    islem_kodu = islenecek_buyruk[14:12]
+                end
+
+            endcase
+        end
+
+        //TODO: cozulen mikroislemlerle gore hangi islemlerin yapilacagini da ilet. 
+    
+    endcase
     ilerle_cmb = 0;
     simdiki_asama_ns = simdiki_asama_r;
 end
@@ -39,6 +81,7 @@ always @(posedge clk) begin
         if (ilerle_cmb) begin
             simdiki_asama_r <= simdiki_asama_ns;
         end
+        ps_r <= ps_ns
     end
 end
 
